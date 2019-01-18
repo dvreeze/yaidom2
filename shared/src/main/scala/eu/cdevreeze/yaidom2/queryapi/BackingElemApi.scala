3b64abaa39899fa7bb061d745c549aa5080e9eae
Chris de Vreeze <chris.de.vreeze@caiway.net>
2019-01-18 22:58:18 +0100

    Added backing elements to query API

---
+++ shared/src/main/scala/eu/cdevreeze/yaidom2/queryapi/BackingElemApi.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019-2019 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom2.queryapi
+
+import java.net.URI
+
+/**
+ * So-called "backing" element API. It extends trait `ScopedElemApi`, but it is also aware of ancestor elements, document
+ * URI, base URI etc. It is therefore fit as backing element of yaidom2 "dialects", where we often need to query ancestor
+ * elements, base URI, etc. Hence the name `BackingElemApi`. This API knows about XML elements, but it does not know about
+ * nodes other than element nodes.
+ *
+ * @author Chris de Vreeze
+ */
+trait BackingElemApi extends Any with ScopedElemApi {
+
+  type ThisElem <: BackingElemApi
+
+  def findParentElem(p: ThisElem => Boolean): Option[ThisElem]
+
+  def filterAncestorElems(p: ThisElem => Boolean): Seq[ThisElem]
+
+  def findAncestorElem(p: ThisElem => Boolean): Option[ThisElem]
+
+  def filterAncestorElemsOrSelf(p: ThisElem => Boolean): Seq[ThisElem]
+
+  def findAncestorElemOrSelf(p: ThisElem => Boolean): Option[ThisElem]
+
+  /**
+   * Returns the optional base URI, computed from the document URI, if any, and the XML base attributes of the
+   * ancestors, if any.
+   */
+  def baseUriOption: Option[URI]
+
+  /**
+   * The base URI, defaulting to the empty URI if absent
+   */
+  def baseUri: URI
+
+  /**
+   * The optional document URI of the containing document, if any
+   */
+  def docUriOption: Option[URI]
+
+  /**
+   * The document URI, defaulting to the empty URI if absent
+   */
+  def docUri: URI
+
+  /**
+   * The root element
+   */
+  def rootElem: ThisElem
+}
+
+object BackingElemApi {
+
+  type Aux[E] = BackingElemApi {
+    type ThisElem = E
+  }
+}
---
+++ shared/src/main/scala/eu/cdevreeze/yaidom2/queryapi/BackingNodes.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019-2019 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.yaidom2.queryapi
+
+/**
+ * '''Core API''' for element nodes that offer the central `BackingElemApi with HasChildNodesApi` query API. Each element implementation that
+ * knows about expanded names as well as qualified name and that also knows about ancestor elements, should directly or indirectly
+ * implement this API.
+ *
+ * This API is directly implemented by elements that are used as '''backing elements''' in "yaidom dialects".
+ * The yaidom2 dialects use this abstract backing element API, thus allowing for multiple backing element
+ * implementation behind an yaidom2 XML dialect.
+ *
+ * Efficient implementations are possible for indexed elements and Saxon NodeInfo objects (backed by Saxon native tiny trees).
+ * Saxon-backed elements are not offered by core yaidom2, however. Saxon tiny trees are attractive for their low memory
+ * footprint and querying performance.
+ *
+ * @author Chris de Vreeze
+ */
+object BackingNodes {
+
+  /**
+   * Arbitrary node
+   */
+  trait Node extends Any with ScopedNodes.Node
+
+  /**
+   * Potential document child, so an element, processing instruction or comment
+   */
+  trait CanBeDocumentChild extends Any with Node with ScopedNodes.CanBeDocumentChild
+
+  /**
+   * Arbitrary element node, offering the `BackingElemApi with HasChildNodesApi` element query API.
+   */
+  trait Elem extends Any with CanBeDocumentChild with ScopedNodes.Elem with BackingElemApi with HasChildNodesApi {
+
+    type ThisElem <: Elem
+
+    type ThisNode >: ThisElem <: Node
+  }
+
+  /**
+   * Arbitrary text node
+   */
+  trait Text extends Any with Node with ScopedNodes.Text
+
+  /**
+   * Arbitrary comment node
+   */
+  trait Comment extends Any with CanBeDocumentChild with ScopedNodes.Comment
+
+  /**
+   * Arbitrary processing instruction node
+   */
+  trait ProcessingInstruction extends Any with CanBeDocumentChild with ScopedNodes.ProcessingInstruction
+
+  object Elem {
+
+    /**
+     * This query API type, restricting Node and Elem to the passed type parameters.
+     *
+     * @tparam N The node type
+     * @tparam E The element type
+     */
+    type Aux[N, E] = Elem { type ThisNode = N; type ThisElem = E }
+  }
+}
